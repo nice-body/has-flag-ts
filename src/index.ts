cf0410c92fc758af2b4adc7f7e7cd1eb422783fd
nice-body <neone2011@live.com>
2016-10-20 14:39:13 +0800

    update jsdoc

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,11 @@
 
 /**
  * 检查一个选项是否存在于命令行参数数组中，-- 以后的参数不进行检测
- * @module module:hasFlag
- */
-
-/**
- * 检查一个选项是否存在于命令行参数数组中，-- 以后的参数不进行检测
+ *
+ * @export
  * @param {string} flag 命令行选项
- * @param {string[]} [argv=process.argv] 命令行参数数组
- * @return {boolean} 选项是否存在与参数数组中
+ * @param {Array<string>} [argv=process.argv] 命令行参数数组
+ * @returns {boolean} 选项是否在参数数组中
  */
 export function hasFlag(flag: string, argv: Array<string> = process.argv): boolean {
     const terminatorPos: number = argv.indexOf("--");
