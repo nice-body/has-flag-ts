7ccf6e77ad81b86812922e85fb9819132be9a4be
nice-body <neone2011@live.com>
2016-10-20 14:46:16 +0800

    add a hyphen before the description

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@
  * 检查一个选项是否存在于命令行参数数组中，-- 以后的参数不进行检测
  *
  * @export
- * @param {string} flag 命令行选项
- * @param {Array<string>} [argv=process.argv] 命令行参数数组
- * @returns {boolean} 选项是否在参数数组中
+ * @param {string} flag - 命令行选项
+ * @param {Array<string>} [argv=process.argv] - 命令行参数数组
+ * @returns {boolean}  - 选项是否在参数数组中
  */
 export function hasFlag(flag: string, argv: Array<string> = process.argv): boolean {
     const terminatorPos: number = argv.indexOf("--");
